3c57d23dc30a9f06c7531983673f432f233b091e
Yassine Saddiki <yassine.saddiki@etu.unilim.fr>
2024-02-23 15:51:43 +0100

    Premier commit

---
+++ YassineSaddikiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YassineSaddikiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ YassineSaddikiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace YassineSaddikiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
